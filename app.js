bac3cfe03631c44147a1c44c609724fd5f8bac99
coolsks <47688809+coolsks@users.noreply.github.com>
2020-12-21 21:02:09 +0530

    ex-05
    wired a div to show output

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var divOutput = document.querySelector("#div-output");
 
-
+console.log(divOutput);
 
 btnTranslate.addEventListener("click", clickedEventHandler);
 
 function clickedEventHandler() {
   console.log("clicked!");
+  console.log(txtInput.value);
+  divOutput.innerText = "translated text " + txtInput.value;
+  
 }
