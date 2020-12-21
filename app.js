96413fd3992d022efaccb541603567fbeb85bcbd
coolsks <47688809+coolsks@users.noreply.github.com>
2020-12-21 20:18:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+console.log(btnTranslate);
+// btnTranslate.addEventListener("click", function clickedEventHandler(){
+//   console.log("clicked!");
+// });
+
+// function clickedEventHandler() {
+// }
